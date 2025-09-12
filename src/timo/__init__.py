4e5d3d1f4a3533bba083ae3103f785f8e99ca6d0
Pierre Stratonovitch <pierre.stratonovitch@gmail.com>
2025-09-12 22:36:09 +0100

    imports

--- src/timo/__init__.py
+++ src/timo/__init__.py
@@ -1,3 +1,6 @@
-from timo.dimension import dim, group, Dimension, SingleDimension, GroupedDimension
-from timo.size import size, Size, SingleSize, GroupedSize
+from timo.dimension import dim, group_dim, Dimension, SingleDimension, GroupedDimension
+from timo.size import size, group_size, Size, SingleSize, GroupedSize
 from timo.shape import shape, Shape
+from timo.shapes import shapes, Shapes
+from timo.transform import Transform
+from timo.transform_context import TransformContext
