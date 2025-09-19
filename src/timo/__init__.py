9e08cacc345376a78185e16f1aeb0298bdad5d64
Pierre Stratonovitch <pierre.stratonovitch@gmail.com>
2025-09-19 22:50:48 +0100

    rename

--- src/timo/__init__.py
+++ src/timo/__init__.py
@@ -1,6 +1,6 @@
 from timo.named_axis import name, NamedAxis
 from timo.sized_named_axis import size, SizedNamedAxis
-from timo.named_shape import shape, NamedShape
-from timo.shape_sequence import shapes, ShapeSequence
+from timo.named_shape import shape, NamedShape, Before, After
+from timo.named_shape_sequence import shapes, NamedShapeSequence
 from timo.transform import Transform
 from timo.transform_context import TransformContext
