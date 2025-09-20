99ede31e784922f9ba3d83945be835b70d6fe117
Pierre Stratonovitch <pierre.stratonovitch@gmail.com>
2025-09-20 22:14:03 +0100

    update

--- demo/transforms.py
+++ demo/transforms.py
@@ -9,7 +9,7 @@ i = shape(B, (C, 3), H, W)
 from timo import shapes, Transform, TransformContext
 from timo.transforms.linear import Linear
 from timo.transforms.dyntanh import DynTanH
-from timo.transforms.patch import Patch, square_patch, compbine_patches
+from timo.transforms.patch import Patch, square, compbine_patches
 from timo.transforms.sequential import Sequential
 from timo.transforms.moveaxis import MoveAxis
 from timo import After
@@ -22,7 +22,7 @@ ln1 = Linear(ctx, on=C, to=64, bias=True)
 dh1 = DynTanH(ctx(ln1), on=C, bias=True)
 ln2 = Linear(ctx(dh1), on=C, to=32, bias=True)
 dh2 = DynTanH(ctx(ln2), on=C, bias=True)
-p = Patch(ctx(dh2), on=(H, W), coordinates=compbine_patches(square_patch(1), square_patch(2)), stat="max")
+p = Patch(ctx(dh2), on=(H, W), coordinates=compbine_patches(square(1), square(2)), stat="mean")
 layer = Sequential(ctx, ln1, dh1, ln2, dh2, p)
 
 
