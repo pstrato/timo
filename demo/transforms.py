efc6da243036b18a13eb814dbb814af490711f13
Pierre Stratonovitch <pierre.stratonovitch@gmail.com>
2025-09-21 22:12:32 +0100

    update

--- demo/transforms.py
+++ demo/transforms.py
@@ -7,12 +7,13 @@ i = shape(B, (C, 3), H, W)
 
 # %%
 from timo import shapes, Transform, TransformContext
+from timo.transforms.id import Id
+from timo.transforms.stop_gradient import StopGradient
 from timo.transforms.linear import Linear
 from timo.transforms.dyntanh import DynTanH
 from timo.transforms.patch import Patch, square, compbine_patches
 from timo.transforms.sequential import Sequential
-from timo.transforms.moveaxis import MoveAxis
-from timo import After
+from timo.transforms.thread import Thread
 
 from flax.nnx.rnglib import Rngs
 from flax import nnx
@@ -22,8 +23,14 @@ ln1 = Linear(ctx, on=C, to=64, bias=True)
 dh1 = DynTanH(ctx(ln1), on=C, bias=True)
 ln2 = Linear(ctx(dh1), on=C, to=32, bias=True)
 dh2 = DynTanH(ctx(ln2), on=C, bias=True)
-p = Patch(ctx(dh2), on=(H, W), coordinates=compbine_patches(square(1), square(2)), stat="mean")
-layer = Sequential(ctx, ln1, dh1, ln2, dh2, p)
+e = Sequential(ln1, dh1, ln2, dh2)
+p = Sequential(
+    StopGradient(ctx(e)), Patch(ctx(e), on=(H, W), coordinates=compbine_patches(square(1), square(2)), stat="max")
+)
+
+# layer = Id(ctx)
+layer = Sequential(e, Thread(Id(ctx(e)), p, on=C))
+# layer = e
 
 
 @nnx.jit
