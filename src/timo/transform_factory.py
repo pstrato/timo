dd0810269aa8a7bf904b35ed41f62a5a4e0b09fb
Pierre Stratonovitch <pierre.stratonovitch@gmail.com>
2025-09-25 22:27:05 +0100

    outputs as context inputs

--- src/timo/transform_factory.py
+++ src/timo/transform_factory.py
@@ -22,8 +22,6 @@ class TransformFactory(TransformContext):
 
     def get(self, name, default=...):
         if name == "input_shapes":
-            return self._input_shapes
-        if name == "output_shapes":
             return self._output_shapes
         return self._ctx.get(name, default)
 
@@ -32,13 +30,13 @@ class TransformFactory(TransformContext):
         return self._ctx
 
     @property
-    def input_shapes(self):
+    def transform_input_shapes(self):
         if self._input_shapes is None:
             raise ValueError("Transform shape not set")
         return self._input_shapes
 
     @property
-    def output_shapes(self):
+    def transform_output_shapes(self):
         if self._output_shapes is None:
             raise ValueError("Transform shape not set")
         return self._output_shapes
@@ -50,14 +48,14 @@ class TransformFactory(TransformContext):
         return Param(self.initializer(self, kind, default_init)(self.rngs.params(), shape))
 
     def in_size(self, on: str | NamedAxis):
-        return self.input_shapes.single_shape()[on].set_size
+        return self.transform_input_shapes.single_shape()[on].set_size
 
     def to_size(self, on: str | NamedAxis):
-        return self.output_shapes.single_shape()[on].set_size
+        return self.transform_output_shapes.single_shape()[on].set_size
 
     def vmap(self, function: callable, non_mapped_args: tuple, *on: str | NamedAxis):
-        for _ in self.input_shapes.single_shape().before(*on):
+        for _ in self.transform_input_shapes.single_shape().before(*on):
             function = vmap(function, in_axes=(0, *non_mapped_args), out_axes=0)
-        for _ in self.input_shapes.single_shape().after(*on):
+        for _ in self.transform_input_shapes.single_shape().after(*on):
             function = vmap(function, in_axes=(-1, *non_mapped_args), out_axes=-1)
         return function
