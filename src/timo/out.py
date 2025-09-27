2c4aec585d4624f53d0a246bfa194b3fa86eca3d
Pierre Stratonovitch <pierre.stratonovitch@gmail.com>
2025-09-27 23:13:06 +0100

    out losses

--- src/timo/out.py
+++ src/timo/out.py
@@ -1,2 +1,20 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from jax import Array
+
+
 class Out:
-    pass
+    def __init__(self):
+        self._losses: dict[tuple, Array] = {}
+
+    def add_loss(self, loss: Array, **keys):
+        keys = tuple(keys.items())
+        self._losses[keys] = self._losses.get(keys, 0) + loss
+
+    def clear(self):
+        self._losses.clear()
+
+    def loss_sum(self):
+        return sum(self._losses.values())
